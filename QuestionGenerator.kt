ff5948d4bff40df98d8d0f648f776a23ae1821b7
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2019-01-29 20:35:26 -0700

    Add question generator

---
+++ QuestionGenerator.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+/**
+ * A short script to format questions nicely
+ * Questions should be written on each line:
+ * question; answer; answerType
+ */
+fun main(args: Array<String>) {
+    var questionId = 91
+    var answerId = 93
+    var questions = mutableListOf<String>()
+    var answers = mutableListOf<String>()
+    val type = "QuestionType.MISC"
+    File("./src/input.dat").forEachLine {
+        val inputs = it.split("; ")
+        questions.add(
+                "Question($questionId, $type, \"${inputs[0]}\", $answerId),"
+        )
+        answers.add(
+                "Answer($answerId, AnswerType.${inputs[2]}, text = \"${inputs[1]}\"),"
+        )
+        questionId++
+        answerId++
+    }
+    println("Copy these into allQuestions")
+    for (question in questions) {
+        println(question)
+    }
+    println("Copy these into allAnswers")
+    for (answer in answers) {
+        println(answer)
+    }
+}
